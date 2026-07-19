beffe945b33ee76689fe989d6709f6fd40f9edd3
40closs-cmd <40closs@gmail.com>
2026-07-19 13:20:24 +0300

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,3 @@
 window.onload = () => {
-  YaSendSuggestToken("https://oauth-master-class-git-main-chitoitssu.vercel.app/");
+  YaSendSuggestToken("https://oauth-master-class-nine-delta.vercel.app/");
 };
